50880ce1862f881499473b4b0b1a46509ba4154f
Scott Wegley <scbrwegley@protonmail.ch>
2023-08-31 13:13:55 -0700

    implement a prime factor function

--- HappyPrimes/PrimeNumber.java
+++ HappyPrimes/PrimeNumber.java
@@ -1,5 +1,7 @@
 package HappyPrimes;
 
+import java.util.ArrayList;
+
 public class PrimeNumber {
     public static boolean isPrime(int p) {
         for (long i = 2L; i <= Math.sqrt(p); i++) {
@@ -10,8 +12,37 @@ public class PrimeNumber {
         return p != 1L && p != 0L;
     }
 
+    /**
+     * Modified algorithm derived from work of Vishwas Garg.
+     * @param p The number to return the prime factors of.
+     * @return An array of integer factors of p.
+     * @throws IllegalArgumentException If the number is 0 or less it's an Illegal Argument.
+     */
     public static int[] primeFactors(int p) throws IllegalArgumentException {
-        return new int[] {};
+        if (p <= 0)
+            throw new IllegalArgumentException("Numbers less than or equal to 0 are Illegal Arguments.");
+        ArrayList<Integer> factors = new ArrayList<>();
+        while (p % 2 == 0) {
+            p *= 0.5;
+            factors.add(2);
+        }
+
+        for (int i = 3; i <= Math.sqrt(p); i += 2) {
+            while (p % i == 0) {
+                p /= i;
+                factors.add(i);
+            }
+        }
+
+        if (p != 2) {
+            factors.add(p);
+        }
+
+        int[] out = new int[factors.size()];
+        for (int i = 0; i < out.length; i++) {
+            out[i] = factors.get(i);
+        }
+        return out;
     }
 
 }
