64aa772447e52a99f53e77b1ccf3c6c98fc80538
Scott Wegley <scbrwegley@protonmail.ch>
2023-08-31 09:47:32 -0700

    added test primes

--- MersennePrimes/Main.java
+++ MersennePrimes/Main.java
@@ -2,7 +2,7 @@ package MersennePrimes;
 
 public class Main {
     public static void main(String[] args) {
-        long[] testNums = new long[]{};
+        long[] testNums = new long[]{3,7,31,127,8,0,-6,5,2};
         for (long num : testNums) {
             System.out.println(num + " M Prime Status: " + MersennePrime.isPrime(num));
         }
