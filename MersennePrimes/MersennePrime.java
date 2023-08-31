68394271fb8c8add4bde8594e7279da37170d6a3
Scott Wegley <scbrwegley@protonmail.ch>
2023-08-31 09:52:09 -0700

    empty isPrime

--- MersennePrimes/MersennePrime.java
+++ MersennePrimes/MersennePrime.java
@@ -4,4 +4,9 @@ public class MersennePrime {
     public static boolean isMPrime(long n) {
         return false;
     }
+
+    public static boolean isPrime(long n){
+        return false;
+    }
+
 }
