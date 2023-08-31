58f951f47bc9dee54ada10a7a7d98826239d3188
Scott Wegley <scbrwegley@protonmail.ch>
2023-08-31 10:18:41 -0700

    powOfTwo function using bitset

--- MersennePrimes/MersennePrime.java
+++ MersennePrimes/MersennePrime.java
@@ -13,6 +13,19 @@ public class MersennePrime {
         }
         return p != 1L && p != 0L;
     }
+
+    public static boolean isPowOfTwo(long p) {
+        long q = p;
+        long r = 0;
+        int bitCount = 0;
+        for (int i = 0; i < 64 && bitCount <= 1; i++) {
+            r = q & 0x01;
+            if (r == 1)
+                bitCount++;
+            q = q >>> 1;
+        }
+        return bitCount <= 1;
+    }
     }
 
 }
