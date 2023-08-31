5d2c0902e5473cfaf53010cc657d13040fbdb8a1
Scott Wegley <scbrwegley@protonmail.ch>
2023-08-31 10:17:54 -0700

    expanded testing list

--- MersennePrimes/Main.java
+++ MersennePrimes/Main.java
@@ -2,9 +2,9 @@ package MersennePrimes;
 
 public class Main {
     public static void main(String[] args) {
-        long[] testNums = new long[]{3,7,31,127,8,0,-6,5,2};
+        long[] testNums = new long[]{3,7,31,127,8,0,-6,5,2, 19, 2305843009213693951L};
         for (long num : testNums) {
-            System.out.println(num + " M Prime Status: " + MersennePrime.isMPrime(num));
+            System.out.println(num + " ||" + MersennePrime.isMPrime(num));
         }
     }
 }
