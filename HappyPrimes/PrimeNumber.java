efe187c95e6f19e986178e42cf6dfc37961db783
Scott Wegley <scbrwegley@protonmail.ch>
2023-08-31 12:52:30 -0700

    added isPrime function

--- HappyPrimes/PrimeNumber.java
+++ HappyPrimes/PrimeNumber.java
@@ -1,5 +1,17 @@
 package HappyPrimes;
 
 public class PrimeNumber {
-    
+    public static boolean isPrime(int p) {
+        for (long i = 2L; i <= Math.sqrt(p); i++) {
+            if (p % i == 0) {
+                return false;
+            }
+        }
+        return p != 1L && p != 0L;
+    }
+
+    public static int[] primeFactors(int p) throws IllegalArgumentException {
+        return new int[] {};
+    }
+
 }
